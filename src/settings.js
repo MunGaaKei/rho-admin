937685280b228c25a3438b47b84553a0711c42a7
iann <502975759@qq.com>
2022-01-07 18:42:20 +0800

    init

--- src/settings.js
+++ src/settings.js
@@ -2,7 +2,7 @@ export const APP_NAME = "Daphne odera";
 
 export const APP_TITLE = "后台系统";
 
-export const APP_LOGO = "https://cdn-icons-png.flaticon.com/128/26/26103.png";
+export const APP_LOGO = "./src/assets/logo.png";
 
 export const ON_PERMISSION = true;
 
